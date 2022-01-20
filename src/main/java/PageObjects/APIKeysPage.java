5f8ff818eccbedec150a8f377f0d19fdf4c3e1ec
phuocthanhpt <phuocthanhpt@gmail.com>
2022-01-20 11:42:41 +0700

    update new code

--- src/main/java/PageObjects/APIKeysPage.java
+++ src/main/java/PageObjects/APIKeysPage.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class APIKeysPage {
@@ -15,13 +16,27 @@ public class APIKeysPage {
     public By inputKey_textField = By.xpath("//input[@id='api_key_form_name']");
     public By generate_button = By.xpath("//input[@name='commit']");
 
-
-
     public APIKeysPage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(this.driver, 10L);
     }
 
+    public APIKeysPage enterKey(String key){
+        wait.until(ExpectedConditions.presenceOfElementLocated(inputKey_textField)).sendKeys(key);
+        return this;
+    }
 
+    public void clickGenerateButton(){
+        wait.until(ExpectedConditions.elementToBeClickable(generate_button)).click();
+    }
 
+    public void generateNewKey(String key){
+        this.enterKey(key)
+                .clickGenerateButton();
+    }
+
+    public void clickDeleteKey(String key){
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//td[normalize-space()='"+ key +"']//following-sibling::td[2]//a[@class='edit_key_btn'][2]")));
+    }
 }
--- src/test/java/test_cases/WeatherMapTest.java
+++ src/test/java/test_cases/WeatherMapTest.java
@@ -1,13 +1,12 @@
 package test_cases;
 
-import PageObjects.LoginPage;
-import PageObjects.Step1Page;
-import PageObjects.Step2Page;
-import PageObjects.WeatherMapHomePage;
+import PageObjects.*;
 import base.BaseTest;
 import com.aventstack.extentreports.Status;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
@@ -22,6 +21,7 @@ public class WeatherMapTest extends BaseTest {
 
     private LoginPage loginPage;
     private WeatherMapHomePage homePage;
+    private APIKeysPage apiKeysPage;
 
     @BeforeClass
     @Parameters({"runMode", "browser"})
@@ -33,6 +33,7 @@ public class WeatherMapTest extends BaseTest {
             wait = new WebDriverWait(driver, 10);
             loginPage = new LoginPage(driver);
             homePage = new WeatherMapHomePage(driver);
+            apiKeysPage = new APIKeysPage(driver);
 
         } catch (Exception e) {
             log.error("setUp() Error occurs: " + e.getMessage() + "\n StackTrace: " + Arrays.toString(e.getStackTrace()));
@@ -77,16 +78,57 @@ public class WeatherMapTest extends BaseTest {
     }
 
     @Test
-    @Parameters({"test_url", "username", "password"})
-    public void tc_CreateAPI(String url, String username, String password) {
+    @Parameters({"test_url", "username", "password", "newKey"})
+    public void tc_CreateAPI(String url, String username, String password, String newKey) {
         try {
             log.info("Go to Login screen");
             loginPage.accessLoginPage(url);
             loginPage.doLogin(username, password);
             Thread.sleep(2000);
             homePage.goToAPIKeysScreen();
+            Thread.sleep(1000);
 
-            Thread.sleep(5000);
+            log.info("Create new key");
+            apiKeysPage.generateNewKey(newKey);
+
+            Thread.sleep(1000);
+
+            // Verify new key has created
+            String log = "Check new key '" + newKey + "' created success";
+            CustomListeners.testReport.get().log(Status.INFO, log);
+            WebElement newKeyElement = driver.findElement(By.xpath("//td[normalize-space()='"+ newKey +"']"));
+            verifyEquals(true, newKeyElement.getText().equals(newKey));
+
+        } catch (Exception e) {
+            finalTestResult = false;
+            e.printStackTrace();
+            String error = "tc_loginSuccessfulWithValidUser - Error occurs: " + e.getMessage();
+            log.error(error);
+            CustomListeners.testReport.get().log(Status.FAIL, error);
+        }
+    }
+
+    @Test
+    @Parameters({"test_url", "username", "password", "newKey"})
+    public void tc_DeleteKey(String url, String username, String password, String newKey) {
+        try {
+            log.info("Go to Login screen");
+            loginPage.accessLoginPage(url);
+            loginPage.doLogin(username, password);
+            Thread.sleep(2000);
+            homePage.goToAPIKeysScreen();
+            Thread.sleep(1000);
+
+            log.info("Create new key");
+            apiKeysPage.generateNewKey(newKey);
+
+            Thread.sleep(1000);
+
+            // Verify new key has created
+            String log = "Check new key '" + newKey + "' created success";
+            CustomListeners.testReport.get().log(Status.INFO, log);
+            WebElement newKeyElement = driver.findElement(By.xpath("//td[normalize-space()='"+ newKey +"']"));
+            verifyEquals(true, newKeyElement.getText().equals(newKey));
 
         } catch (Exception e) {
             finalTestResult = false;
